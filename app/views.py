57feeb53e6f9fbd88084751f35f10f535b4178bd
josep <josep@ROY>
2022-08-07 18:11:27 +0530

    main

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from urllib import request
+from django.shortcuts import render
+from  django.http import HttpResponse
+# Create your views here.
+def home(request):
+    return HttpResponse("<h1> hello Home </h1>")
