922ff0d14295508121097820e0cf88b68674394a
josep <josep@ROY>
2022-08-07 19:48:19 +0530

    add teplates folder

--- django3/settings.py
+++ django3/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r06!(3)tj4+vflxv**n0wzj6dz-ftaoac0rdz8gnetavz&g&!1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['djangojose1.herokuapp.com','*']
