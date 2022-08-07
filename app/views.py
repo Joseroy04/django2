5966b9acf3f8e1bb925ab44fb104b1b87ad9ba06
josep <josep@ROY>
2022-08-07 19:29:13 +0530

    adding templatews files > homepage

--- app/views.py
+++ app/views.py
@@ -4,3 +4,6 @@ from  django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("<h1> hello Home </h1>")
+def home2(request):
+    homepage='homepage.html'
+    return render(request,homepage,{})
